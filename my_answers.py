13a8682477c738f49ebc84a995298a0657914e66
fmind <fmind@users.noreply.github.com>
2017-08-27 17:23:16 +0200

    Implemented window transform series

--- my_answers.py
+++ my_answers.py
@@ -1,46 +1,63 @@
+# import keras
 import numpy as np
 
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-import keras
+# from keras.layers import LSTM, Dense
+# from keras.models import Sequential
 
 
-# TODO: fill out the function below that transforms the input series 
+# TODO: fill out the function below that transforms the input series
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
-    # containers for input/output pairs
-    X = []
-    y = []
+    """Containers for input/output pairs based on window size.
+    >>> X, y = window_transform_series(np.array([1, 3, 5, 7, 9, 11, 13]), 2)
+    >>> X
+    array([[ 1,  3],
+           [ 3,  5],
+           [ 5,  7],
+           [ 7,  9],
+           [ 9, 11]])
+    >>> y
+    array([[ 5],
+           [ 7],
+           [ 9],
+           [11],
+           [13]])
+    """
+    n = len(series)
+    y = [[x] for x in series[window_size:]]
+    X = [series[i:i + window_size] for i in range(0, n - window_size)]
 
-    # reshape each 
+    # reshape each
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
-    y.shape = (len(y),1)
+    y.shape = (len(y), 1)
 
-    return X,y
+    return X, y
+
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
     pass
 
 
-### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+# TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
-    punctuation = ['!', ',', '.', ':', ';', '?']
+    # punctuation = ['!', ',', '.', ':', ';', '?']
 
     return text
 
-### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
+
+# TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
 
-    return inputs,outputs
+    return inputs, outputs
 
-# TODO build the required RNN model: 
-# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
+
+# TODO build the required RNN model:
+# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss
 def build_part2_RNN(window_size, num_chars):
     pass
